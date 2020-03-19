c161881b3bb7e7a3a988d66b9bd68bd46a33011f
Ricardo Fornes <jrfornes5@gmail.com>
2020-03-19 14:03:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-/* code goes here */
+const fruitsBaseIds = {
+  banana: 1,
+  apple: 2,
+  orange: 3
+};
+
+const fruits = [
+  {
+    type: 'banana',
+    id: undefined
+  },
+  {
+    type: 'banana',
+    id: undefined
+  },
+  {
+    type: 'apple',
+    id: undefined
+  },
+  {
+    type: 'apple',
+    id: undefined
+  },
+  {
+    type: 'orange',
+    id: undefined
+  }
+];
+
+const result = fruits.reduce( (accumulator, currentValue) => {
+  console.log(currentValue)  
+}, []);
+
+console.log(result)
